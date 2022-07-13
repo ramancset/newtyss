f4de78bc87fdfb2cf9e4761eebfc523eb013d008
Raman Jha <Raman Jha@DESKTOP-17PQN8B>
2022-07-13 17:58:02 +0530

    new commit

---
+++ src/main/java/Raman_Mavan/democlass.java
@@ -0,0 +1,10 @@
+package Raman_Mavan;
+
+public class democlass
+{
+	public String sample()
+	{
+		return "sample";
+	}
+
+}
---
+++ src/test/java/Raman_Maven/sample.java
@@ -0,0 +1,14 @@
+package Raman_Maven;
+
+import org.testng.annotations.Test;
+
+public class sample 
+{
+
+@Test
+	public void test() 
+    {
+	System.out.println("raman");
+	}
+
+}
